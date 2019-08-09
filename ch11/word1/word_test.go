0ce5faeabfdd7117e890e8f70b1cc3b608ddde5c
arimura <33759663+arimura@users.noreply.github.com>
2019-08-10 07:16:19 +0900

    Add

---
+++ ch11/word1/word_test.go
@@ -0,0 +1,24 @@
+package word
+
+import "testing"
+
+func TestIsPalindrome(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"", true},
+		{"a", true},
+		{"aa", true},
+		{"ab", false},
+		{"kayak", true},
+		{"detartrated", true},
+		{"A man, a plan, a canal: Panama", true},
+		{"été", true},
+	}
+	for _, test := range tests {
+		if got := IsPalindrome(test.input); got != test.want {
+			t.Errorf("IsPalindrome(%q) = %v", test.input, got)
+		}
+	}
+}
